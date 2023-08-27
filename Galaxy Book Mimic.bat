89394f3e0a1dacd8aa28e98b027e4919d5c54e7d
kellwinr <96328543+kellwinr@users.noreply.github.com>
2023-08-27 22:49:56 +0800

    Add files via upload

---
+++ Galaxy Book Mimic.bat
@@ -0,0 +1,5 @@
+set "params=%*" && cd /d "%~dp0" && pushd "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemProductName /t REG_SZ /d "NP950QDB-KA1US" /f
+reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemManufacturer /t REG_SZ /d "Samsung" /f
+copy "samsung_notes.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" /Y
+exit
